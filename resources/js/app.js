86ba23739a669fbe4de53c6fcab97e4546ff425e
rafaelleitedasilva <rafael.leite.14@hotmail.com>
2025-06-13 16:06:19 -0300

    App.js

---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+import './bootstrap';
+// Create an instance of CircleProgress element
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import.meta.glob([ '../favicon/**' ]);
+import.meta.glob([ '../images/**' ]);
+import.meta.glob([ '../gif/**' ]);
+
